34020cfc6f71c3904aeae0924f27970ffd75eb26
figueiredocamila <camiladiasfigueiredo@gmail.com>
2020-09-25 22:50:48 -0300

    buttons created

---
+++ src/components/atoms/Button/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { StyledSearchButton, StyledCityButton, StyledIconButton } from './style';
+
+const Button = props => {
+  
+  return (
+    <>
+      {props.type === 'search' && <StyledSearchButton onClick={() => props.onclick()}>Search</StyledSearchButton>}
+      {props.type === 'city' && <StyledCityButton onClick={() => props.onclick()}>{props.text}</StyledCityButton>}
+      {props.type === 'icon' && <StyledIconButton onClick={() => props.onclick()}>
+        <FontAwesomeIcon icon={faTimes} size="2x" color="red" /> 
+      </StyledIconButton>}
+    </>
+  )
+}
+
+export default Button;
---
+++ src/components/atoms/Button/style.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const StyledSearchButton = styled.button`
+  width: 100%;
+  height: 40px;
+  border-radius: 50px;
+  border: 1px solid white;
+  font-size: 1.5em;
+  color: white;
+  background-color: grey;
+`
+
+export const StyledCityButton = styled.button`
+  width: 100%;
+  height: 60px;
+  border-radius: 4px;
+  border: 1px solid grey;
+  font-size: 2em;
+  color: grey;
+  text-align: justify;
+  background-color: white;
+`
+
+export const StyledIconButton = styled.button`
+width: 30px;
+height: 30px;
+border: none;
+background-color: transparent;
+`
